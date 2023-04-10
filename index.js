b50fde15589cd3b7e95f11d0de9058a308aaf4d8
amr mohamed <amrmohamed09@gmail.com>
2023-04-10 22:27:22 +0200

    initial commits

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const cors = require("cors");
 const mongoose = require("mongoose");
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-const upload = require('./utilies/multer')
+const upload = require("./utilies/multer");
 
-app.post('/', upload.Multer.single('image'), async(req,res)=>{
-  const file = req.file.originalname
+app.post("/", upload.Multer.single("image"), async (req, res) => {
+  const file = req.file.originalname;
   console.log(req.file);
-  const result = await cloudinary.uploader.upload(req.file.path,{
-    folder:'products',
-  })
+  const result = await cloudinary.uploader.upload(req.file.path, {
+    folder: "products",
+  });
   console.log(result);
-  res.json(result)
-})
-mongoose.connect("mongodb+srv://amrmohamed09:CgmhOWTLnga6hrk2@cluster0.py6tdm5.mongodb.net/test", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  res.json(result);
 });
-const categoryRoutes = require('./routes/categoryRoutes')
-const authRoutes = require('./routes/authRoutes')
-const productRoutes = require('./routes/productRoutes')
-app.use('/auth', authRoutes)
-app.use('/product', productRoutes)
-app.use('/category', categoryRoutes)
-app.listen(8000, console.log('server is running on port 8000'))
+  mongoose.connect("mongodb+srv://amrmohamed09:CgmhOWTLnga6hrk2@cluster0.py6tdm5.mongodb.net/test", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  
+const categoryRoutes = require("./routes/categoryRoutes");
+const authRoutes = require("./routes/authRoutes");
+const productRoutes = require("./routes/productRoutes");
+app.use("/auth", authRoutes);
+app.use("/product", productRoutes);
+app.use("/category", categoryRoutes);
+app.listen(8000, console.log("server is running on port 8000"));

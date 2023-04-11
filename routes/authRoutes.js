e4b22206c8804ff5aa854c0d95cf25389f78eee7
AmrMhamed <110258591+AmrMhamed@users.noreply.github.com>
2023-04-11 02:01:01 +0200

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const { signUp, signIn, update } = require('../controller/userController')
 const upload = require('../utilies/multer')
-router.post('/signup'  ,  signUp)
+router.post('/signup'  , upload.Multer.single('image'), signUp)
 router.post('/signin', signIn)
 router.post('/update', upload.Multer.single('image'), update)
 module.exports = router;

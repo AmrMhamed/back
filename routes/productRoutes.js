ada44032cb3981e2cd3d427dc6a6b701ca95da36
AmrMhamed <110258591+AmrMhamed@users.noreply.github.com>
2023-04-11 02:27:43 +0200

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router()
-const { addProduct, getProduct, deleteProduct, getBySection, getByCategory } = require('../controller/productController')
+const { addProduct, getProduct, deleteProduct, getBySection, getByCategory, getProducts } = require('../controller/productController')
 const upload = require('../utilies/multer')
 
 router.post('/add', upload.Multer.single('image'), addProduct)
 router.get('/get/:id' , getProduct)
+router.get('/' , getProducts)
 router.post('/delete' , deleteProduct)
 router.post('/getBySection', getBySection)
 router.post('/getByCategory', getByCategory)
